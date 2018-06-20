94d4691ed42df003f2ffece2e96df8342b27cb95
Bernd Lehmann <bernd@lehmann.be>
2018-06-20 20:01:15 +0200

    added dns and ntp

--- lib/filter_plugins/filter.py
+++ lib/filter_plugins/filter.py
@@ -4,9 +4,17 @@
 def compile_node_ips(ip_list):
     return [{"ip": {"addr": x, "type": "V4"}, "name": x} for x in ip_list]
 
+def compile_node_dns(dns_list):
+    return [ {"addr": x, "type": "V4"} for x in dns_list]
+
+def compile_node_ntp(ntp_list):
+    return [{"server": {"addr": x, "type": "DNS"}} for x in ntp_list]
+
 class FilterModule(object):
     def filters(self):
         return {
-            'compile_node_ips'       : compile_node_ips
+            'compile_node_ips'       : compile_node_ips,
+            'compile_node_dns'       : compile_node_dns,
+            'compile_node_ntp'       : compile_node_ntp
         }
 
